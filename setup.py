ba73fff1af80a489522b9082a4578ef69ec13a09
Marcelo Lima <mllimabox-geral@yahoo.com.br>
2019-11-17 17:55:09 -0300

    dependence removed

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='opom',
       version='0.0.1',
-      install_requires=['numpy', 'scipy', 'math']  # All dependencies
+      install_requires=['numpy', 'scipy']  # All dependencies
 )
