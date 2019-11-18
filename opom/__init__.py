0e08011aac45af25b5f3981f061ab18002dc9473
Marcelo Lima <mllimabox-geral@yahoo.com.br>
2019-11-17 21:30:49 -0300

    to make the import easier

---
+++ opom/__init__.py
@@ -0,0 +1,1 @@
+from opom.opom import *
--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='opom',
       version='0.0.1',
+      packages=['opom'],
       install_requires=['numpy', 'scipy']  # All dependencies
 )
